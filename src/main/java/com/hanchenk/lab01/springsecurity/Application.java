c3aac019ef20ce23b237227a40e05625426c6730
Hanchenk <3384623206@qq.com>
2024-12-13 09:08:08 +0800

    Initial commit

---
+++ src/main/java/com/hanchenk/lab01/springsecurity/Application.java
@@ -0,0 +1,9 @@
+package com.hanchenk.lab01.springsecurity;/**
+ *ClassName:Application
+ *Package: com.hanchenk.lab01.springsecurity  
+ *Description:
+ *@Author:Hanchenk
+ *@Create:2024/12/13 - 8:50
+ *@Version: v1.0 
+*/public class Application {
+}
---
+++ src/main/java/com/hanchenk/lab01/springsecurity/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.hanchenk.lab01.springsecurity.controller;/**
+ *ClassName:AdminController
+ *Package: com.hanchenk.lab01.springsecurity.controller  
+ *Description:
+ *@Author:Hanchenk
+ *@Create:2024/12/13 - 8:52
+ *@Version: v1.0 
+*/public class AdminController {
+}
